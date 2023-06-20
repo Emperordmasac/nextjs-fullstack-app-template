a20a064d8d15bbda179bf73d7701bf6e21de2f7f
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-06-20 14:17:02 +0100

    build: implement storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
+
+// module.exports = {
+//   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+//   /** Expose public folder to storybook as static */
+//   staticDirs: ['../public'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/addon-interactions',
+//   ],
+//   framework: '@storybook/react',
+//   core: {
+//     builder: '@storybook/builder-webpack5',
+//   },
+// };
