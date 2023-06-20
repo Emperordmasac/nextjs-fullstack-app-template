7b9b805a1b13960a4f4911c310df3b27129d0c24
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-06-20 14:51:44 +0100

    build: create BaseTemplate component

---
+++ src/components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryFn } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+export default {
+  title: 'templates/BaseTemplate',
+  component: BaseTemplate,
+  //---More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as Meta<typeof BaseTemplate>;
+//--ComponentMeta depreceated --> Meta
+
+//---More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+const Template: StoryFn<typeof BaseTemplate> = (args) => (
+  <BaseTemplate {...args} />
+);
+
+//--ComponentStory depreceated -> StoryFn
+
+export const Base = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+Base.args = {
+  ...mockBaseTemplateProps.base,
+} as IBaseTemplate;
